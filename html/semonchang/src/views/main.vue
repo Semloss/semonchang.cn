988857f72ec21efdd48a9c81ca30ff04844a8fa3
semonchang <445671079@qq.com>
2019-03-27 18:26:56 +0800

    add html

---
+++ html/semonchang/src/views/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="nav"></div>
+    <div class="body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainpage",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+.container {
+  height: 100vh;
+  width: 100vw;
+  background: #ffffff
+    url("../../../../../../../Desktop/isabela-ferreira-780666-unsplash.jpg")
+    center no-repeat;
+  background-size: 100%;
+}
+</style>
