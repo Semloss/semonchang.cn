b92478334490f3c2aa8078ab9b51e26e02292639
Semon <445671079@qq.com>
2019-04-24 11:10:40 +0800

    modify

--- html/src/views/main.vue
+++ html/src/views/main.vue
@@ -1,46 +1,18 @@
 <template>
   <div class="container">
-    <div class="nav">
-      <img
-        src="https://img.hellobyebye.com/fd48916c4b133a983116f1a693fa01e2.jpg"
-        alt=""
-        class="logo"
-      />
-      <p class="login" @click="dialogFormVisible = true">LOGIN</p>
+    <div class="login">
+      <p class="logintext" @click="dialogFormVisible = true">Login</p>
     </div>
-    <div class="body"></div>
-    <el-dialog title="登录" :visible.sync="dialogFormVisible" width="30%">
-      <el-form :model="form">
-        <el-form-item label="用户名" :label-width="formLabelWidth">
-          <el-input v-model="form.username" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" :label-width="formLabelWidth">
-          <el-input v-model="form.passwd" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="login()">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import * as api from "../api/login";
-import axios from "axios";
+// import axios from "axios";
 export default {
-  name: "mainpage",
+  name: "mainPage",
   data() {
-    return {
-      dialogFormVisible: false,
-      form: {
-        username: "",
-        passwd: ""
-      },
-      formLabelWidth: "60px",
-      loginSuccess: false
-    };
+    return {};
   },
   methods: {
     async login() {
@@ -64,32 +36,29 @@ export default {
   height: 100vh;
   width: 100vw;
   background: #ffffff
-    url("https://img.hellobyebye.com/54268b7fab123bd05303bf21a773b3a0.jpg")
+    url("https://img.hellobyebye.com/9e2a6d4b9a6345e17889c19ee2002935.jpg")
     center no-repeat;
   /* background-size: 100%; */
+  position: relative;
 
-  .nav {
-    height: 60px;
-    background-color: rgba(0, 0, 0, 0.1);
-    position: relative;
-
-    .logo {
-      width: 10%;
-      height: 100%;
-      position: absolute;
-      left: 2%;
-    }
-    .login {
-      color: #ffffff;
+  .login {
+    background-color: #c21f5b;
+    height: 50px;
+    width: 100px;
+    position: absolute;
+    right: 5%;
+    top: 25%;
+    border-radius: 25px;
+    line-height: 50px;
+    right: 15%;
+    top: 5%;
+    .logintext {
       font-size: 1.3rem;
-      position: absolute;
-      right: 5%;
-      top: 25%;
+      color: #ffffff;
     }
-    .login:hover {
-      color: #c0c4cc;
-      cursor: pointer;
-    }
+  }
+  .login:hover {
+    cursor: pointer;
   }
 }
 </style>
